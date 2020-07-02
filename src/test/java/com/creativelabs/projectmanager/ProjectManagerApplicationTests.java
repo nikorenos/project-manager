ccff6c3537bbc9e32c1cf32f11a5f6b804ad91f6
Sentarion <mbalabox@gmail.com>
2020-07-03 01:53:03 +0200

    first commit

---
+++ src/test/java/com/creativelabs/projectmanager/ProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creativelabs.projectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
